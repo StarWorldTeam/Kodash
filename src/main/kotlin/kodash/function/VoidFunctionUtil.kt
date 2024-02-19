22735d50ab2178c6639696eb0ab32c303354a3cd
WowStarWorld <starworldstudio1@gmail.com>
2024-02-19 09:23:11 +0800

    [update] 修正memorize代码

--- src/main/kotlin/kodash/function/VoidFunctionUtil.kt
+++ src/main/kotlin/kodash/function/VoidFunctionUtil.kt
@@ -18,7 +18,8 @@ private val memory: MutableMap<String, Any?> = mutableMapOf()
 fun <T> memorize(vararg seeds: Any?, block: () -> T): T {
     val stack = Throwable().stackTrace
     val caller = stack.getOrNull(1) ?: return block()
-    val key = "seed=${Objects.hashCode(*seeds)},${seeds.size},${seeds::class.java.arrayType().name};module=${caller.moduleName}@${caller.moduleVersion};" +
+    val key = "seed=${Objects.hashCode(*seeds)},${seeds.size},${seeds::class.java.arrayType().name};" +
+            "module=${caller.moduleName}@${caller.moduleVersion};" +
             "classLoader=${caller.classLoaderName};" +
             "method=${caller.className}::${caller.methodName};" +
             "native=${caller.isNativeMethod};" +
