39736bbf45d03fa1be078ab74f7ba909dec76dab
WowStarWorld <starworldstudio1@gmail.com>
2024-03-10 19:02:37 +0800

    [fix] 修复jackson报错问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.3")
     implementation("com.fasterxml.jackson.core:jackson-core:2.15.3")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.3")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
